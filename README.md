bea656a42bd80add7eb9eee286c123e71f6bd0c9
Vo Tuan Kiet <votuankiet1011@gmail.com>
2024-02-20 21:06:54 +0700

    Add features

--- README.md
+++ README.md
@@ -37,3 +37,25 @@ streamlit run app.py
 ```
 
 This will start the Streamlit server and open the application in your default web browser.
+
+## Default Configuration:
+
+- The default Language Model (LLM) is set to `ChatFireworks` with the default model being `mistral-7b-instruct`. However, you can customize the LLM according to your needs in the `llm.py` file.
+    - **vllm** : Uncomment `vllm` session in `llm.py`, then run the scripts below
+    ```shell
+    pip install vllm
+    python -m vllm.entrypoints.api_server \
+    --model MODELNAME\
+    ```
+
+    - **Llamacpp** : Uncomment `Llamacpp` session in `llm.py`, then run the scripts below
+    ```shell
+    CMAKE_ARGS="-DLLAMA_CUBLAS=on" FORCE_CMAKE=1 pip install llama-cpp-python[server]
+    python3 -m llama_cpp.server --model MODEL_PATH --n_gpu_layers N_GPU_LAYERS --n_ctx N_CTX --chat_format CHAT_FORMAT --host 0.0.0.0 --port 8000
+    ```
+    - **ChatHuggingFace** : Uncomment `ChatHuggingFace` session in `llm.py`, then run the scripts below
+    Add your `HUGGINGFACE_HUB_API_TOKEN` to .env file
+
+- The default embedding function is Nomic embedding. However, if you wish, you can use any embedding function by editing or uncommenting the appropriate sections in the `ingest.py` file.
+
+- The default vector database is Chroma. Nevertheless, you can change the vector database as desired by editing or uncommenting the relevant sections in the `ingest.py` file.
--- llm.py
+++ llm.py
@@ -4,6 +4,9 @@ from langchain.callbacks.manager import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain_openai import OpenAI, ChatOpenAI
 from langchain_community.chat_models.fireworks import ChatFireworks
+from langchain_community.llms import HuggingFaceHub
+from langchain_community.chat_models.huggingface import ChatHuggingFace
+
 
 # LlamaCpp_model_path = 'PATH_TO_/models/llama-2-7b-chat.Q4_K_M.gguf'
 FIREWORKS_MODEL_ID = "accounts/fireworks/models/mistral-7b-instruct-4k"
@@ -15,15 +18,16 @@ class Loadllm:
     def load_llm():
 
         # Create Local LLM using LlamaCpp, uncomment code below to use
-        # callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
-        # llm = LlamaCpp(
-        #     model_path=model_path,
-        #     n_gpu_layers=40,
-        #     n_batch=512,
-        #     n_ctx=2048,
-        #     f16_kv=True,  # MUST set to True, otherwise you will run into problem after a couple of calls
-        #     callback_manager=callback_manager,
-        #     verbose=True,
+
+        # inference_server_url = "http://0.0.0.0:8000"
+        # openai_api_key = "EMPTY"
+        # model_name = "HuggingFaceH4/zephyr-7b-beta"
+        # llm = ChatOpenAI(
+        #     model=model_name,
+        #     openai_api_key=openai_api_key,
+        #     openai_api_base=inference_server_url,
+        #     max_tokens=8192,
+        #     temperature=0,
         # )
 
         # Create Local LLM using vllm, uncomment code below to use 
@@ -37,6 +41,13 @@ class Loadllm:
         #     max_tokens=8192,
         #     temperature=0,
         # )
+
+        # Create Local LLM using ChatHuggingFace,uncomment code below to use 
+        # llm = HuggingFaceHub(
+        #     repo_id="HuggingFaceH4/zephyr-7b-beta",
+        #     task="text-generation",
+        # )
+        # llm = ChatHuggingFace(llm=llm)
 
         # Create chat model using OpenAI, uncomment code below to use
         # llm = ChatOpenAI(temperature=0)
